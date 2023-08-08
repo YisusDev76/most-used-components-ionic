6213b0781025fa310c45e8f4e260a061d8c34129
yisusDev76 <je.parra.navarrete@gmail.com>
2023-08-08 16:59:13 -0600

    se agrego el menu en la carpeta pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
+  },
 ];
---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class MenuPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
