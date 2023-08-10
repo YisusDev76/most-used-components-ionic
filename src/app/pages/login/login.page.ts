0ef25478bcb0f7778cd105417182b2732a84d97e
yisusDev76 <je.parra.navarrete@gmail.com>
2023-08-09 18:50:02 -0600

    bugs corregidos de ayer el cual hacian que no funcionaran los botones de navegación

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class LoginPage implements OnInit {
 
--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { NavigationEnd, Router, RouterEvent, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
 })
 export class MenuPage implements OnInit {
 
@@ -31,6 +31,8 @@ export class MenuPage implements OnInit {
     this.router.events.subscribe((event: any) => { // Aquí estamos utilizando 'any' en lugar de 'RouterEvent'
       if (event instanceof NavigationEnd) {
         this.selectedPath = event.url;
+        console.log("Cambios el seleccionado");
+        
       }
     });
   }
