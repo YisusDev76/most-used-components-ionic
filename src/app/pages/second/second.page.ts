36ae71925da054f7e6eebc170b74dc617fe38a2e
yisusDev76 <je.parra.navarrete@gmail.com>
2023-08-26 22:28:17 -0600

    se realizo una estrurtura de esqueleto de carga y se le agrego una animacion para cuando quieres pasar del esqueleto al contenido real, se tuvo que hacer unos pasos de conf differentes porque estoy usando standalone componentes pero si esta funcionando todo chido

--- src/app/pages/second/second.page.ts
+++ src/app/pages/second/second.page.ts
@@ -3,19 +3,31 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.page.html',
   styleUrls: ['./second.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+  animations:[
+    trigger('fadeInOut',[
+      state('void',style({opacity:0})),
+      state('*', style({opacity:1})),
+      transition('void => *',animate('200ms ease-in')),
+      transition('* => void', animate('200ms ease-out'))
+    ])
+  ],
 })
 export class SecondPage implements OnInit {
-
+  loading = true;
   constructor() { }
 
   ngOnInit() {
+    setTimeout(()=>{
+      this.loading = false;
+    },2000);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from "@angular/common/http";
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -15,6 +16,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
+    importProvidersFrom(BrowserAnimationsModule),
     provideRouter(routes),
     provideHttpClient(),
   ],
