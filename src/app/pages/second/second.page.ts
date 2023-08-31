6742b259a49b3050a3c3ed632d357297ca1f6f52
yisusDev76 <je.parra.navarrete@gmail.com>
2023-08-31 16:44:02 -0600

    primera animacacion de boton creada ya en ionic pero no tiene buenas practicas

--- src/app/pages/second/second.page.ts
+++ src/app/pages/second/second.page.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AnimationController, IonButton } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-second',
@@ -22,12 +25,37 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 })
 export class SecondPage implements OnInit {
   loading = true;
-  constructor() { }
+  @ViewChild('btnIonAcademy', { read: ElementRef }) cartBnt!: ElementRef;
+  private animation: Animation | undefined;
+
+  constructor(private animationCtrl: AnimationController) {}
 
   ngOnInit() {
     setTimeout(()=>{
       this.loading = false;
     },2000);
+  }
+
+  ngAfterViewInit() {
+    const btnIon = this.animationCtrl
+    .create()
+    .addElement(this.cartBnt.nativeElement)
+    .duration(2000)
+    .delay(1000)
+    .keyframes([
+      { offset: 0, transform: 'scale(1)', opacity: '1' },
+      { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
+      { offset: 1, transform: 'scale(1)', opacity: '1' },
+    ]);
+
+    this.animation = this.animationCtrl
+      .create()
+      .duration(2000)
+      .iterations(Infinity)
+      .addAnimation([btnIon]);
+
+    this.animation.play();
+
   }
 
 }
