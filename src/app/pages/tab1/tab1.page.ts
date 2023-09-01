80c573790df702e48c016bcadd34ddb2bacc2bc3
yisusDev76 <je.parra.navarrete@gmail.com>
2023-08-31 19:14:47 -0600

    Se estuvo probando la API de gestos de ionic pero aun le falta hacer comentarios y entender mejor como funciona porque es de las  apis mas complejas de ionic que he manejado hasta ahora

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { GestureController, GestureDetail, IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -13,9 +13,43 @@ import { RouterLink } from '@angular/router';
 })
 export class Tab1Page implements OnInit {
 
-  constructor() { }
+  @ViewChild('square', { read: ElementRef }) square!: ElementRef<HTMLIonCardElement> ;
+
+  constructor(private el: ElementRef, private gestureCtrl: GestureController) { }
 
   ngOnInit() {
   }
 
+
+  ngAfterViewInit() {
+    const gesture = this.gestureCtrl.create({
+      el: this.square.nativeElement,
+      threshold: 0,
+      gestureName: 'my-gesture',
+      onStart:(ev) => this.onStartHandler(ev),
+      onMove:(ev) => this.onMoveHandler(ev),
+      onEnd:(ev) => this.onMoveEnd(ev),
+      // onStart: () => this.onStart(),
+      
+    });
+
+    gesture.enable();
+  }
+
+  private onStartHandler = (detail:GestureDetail)=>{
+    this.square.nativeElement.style.transition = 'none';
+  }
+
+  private onMoveHandler = (detail:GestureDetail) => {
+    // console.log(detail);
+    const x = detail.currentX - detail.startX;
+    const y = detail.currentY - detail.startY;
+
+    this.square.nativeElement.style.transform = `translate(${x}px, ${y}px)`
+  }
+
+  private onMoveEnd = (detail:GestureDetail) => {
+    this.square.nativeElement.style.transition = '500ms ease-out';
+    this.square.nativeElement.style.transform =  `translate(0px,0px)`;
+  }
 }
