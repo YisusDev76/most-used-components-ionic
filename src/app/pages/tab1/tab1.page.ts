0f03e108dfd4f45dc96b3ab289b25876522b5992
yisusDev76 <je.parra.navarrete@gmail.com>
2023-09-01 18:18:37 -0600

    se agregaron comentarios al codigo que hace posible el gesto de movel el cuadrado usando las Ionic gesture API

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -20,7 +20,7 @@ export class Tab1Page implements OnInit {
   ngOnInit() {
   }
 
-
+  // Configurando el gesto después de que la vista ha sido inicializada
   ngAfterViewInit() {
     const gesture = this.gestureCtrl.create({
       el: this.square.nativeElement,
@@ -36,18 +36,30 @@ export class Tab1Page implements OnInit {
     gesture.enable();
   }
 
+  // Desactiva las transiciones cuando el gesto comienza
   private onStartHandler = (detail:GestureDetail)=>{
     this.square.nativeElement.style.transition = 'none';
   }
 
+  /**
+   * Ajusta la transformación del elemento basado en el movimiento del gesto:
+   * Esta función es invocada cada vez que el usuario mueve su punto de contacto (por ejemplo, su dedo en una pantalla táctil).
+   *  Su propósito principal es ajustar la posición del elemento basándose en el movimiento realizado por el usuario.
+   */
+  
   private onMoveHandler = (detail:GestureDetail) => {
     // console.log(detail);
+    // Calculamos la diferencia en el eje X entre la posición actual del punto de contacto y la posición inicial cuando el gesto comenzó.
     const x = detail.currentX - detail.startX;
+     // Hacemos lo mismo para el eje Y.
     const y = detail.currentY - detail.startY;
 
+    // Usamos estas diferencias para mover nuestro elemento. 
+    // Si el usuario ha movido su dedo 10px a la derecha y 5px hacia abajo, el elemento se moverá 10px a la derecha y 5px hacia abajo.
     this.square.nativeElement.style.transform = `translate(${x}px, ${y}px)`
   }
 
+   // Restablece la posición del elemento a su posición original una vez que el gesto ha terminado
   private onMoveEnd = (detail:GestureDetail) => {
     this.square.nativeElement.style.transition = '500ms ease-out';
     this.square.nativeElement.style.transform =  `translate(0px,0px)`;
